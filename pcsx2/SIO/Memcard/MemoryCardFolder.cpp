a8b98e5099b97ac5e5466d865db1af488fe92184
oltolm <oleg.tolmatcev@gmail.com>
2023-10-07 00:05:03 +0200

    make compile with mingw-w64

--- pcsx2/SIO/Memcard/MemoryCardFolder.cpp
+++ pcsx2/SIO/Memcard/MemoryCardFolder.cpp
@@ -72,7 +72,7 @@ static auto last = std::chrono::time_point<std::chrono::system_clock>();
 MemoryCardFileEntryDateTime MemoryCardFileEntryDateTime::FromTime(time_t time)
 {
 	struct tm converted = {};
-#ifdef _MSC_VER
+#ifdef _WIN32
 	gmtime_s(&converted, &time);
 #else
 	gmtime_r(&time, &converted);
@@ -99,7 +99,7 @@ time_t MemoryCardFileEntryDateTime::ToTime() const
 	converted.tm_mon = std::max(static_cast<int>(month) - 1, 0);
 	converted.tm_year = std::max(static_cast<int>(year) - 1900, 0);
 
-#ifdef _MSC_VER
+#ifdef _WIN32
 	return _mkgmtime(&converted);
 #else
 	return timegm(&converted);
