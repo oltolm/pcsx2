caad1a201742368c8df4ddbf7dd883524f995f4a
oltolm <oleg.tolmatcev@gmail.com>
2025-11-01 23:43:57 +0100

    disable CrashHandler on MINGW32

--- pcsx2-qt/QtHost.cpp
+++ pcsx2-qt/QtHost.cpp
@@ -1308,8 +1308,10 @@ bool QtHost::InitializeConfig()
 		return false;
 	}
 
+#ifndef __MINGW32__
 	// Write crash dumps to the data directory, since that'll be accessible for certain.
 	CrashHandler::SetWriteDirectory(EmuFolders::DataRoot);
+#endif
 
 	const std::string path = Path::Combine(EmuFolders::Settings, "PCSX2.ini");
 	const bool settings_exists = FileSystem::FileExists(path.c_str());
@@ -2347,7 +2349,9 @@ public:
 
 int main(int argc, char* argv[])
 {
+#ifndef __MINGW32__
 	CrashHandler::Install();
+#endif
 
 // Exceptions are disabled, so we can't try/catch this.
 // Timestamps in some locales showed up wrong on Windows.
